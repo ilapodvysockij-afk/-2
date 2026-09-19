15cbc2cd7420039420c699d692fb386c7ed5648f
ilapo <ilapodvysockij@gmail.com>
2026-09-19 13:26:51 +0300

    дз2

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-class Human:
-    def __init__(self, name="Human"):
-        self.name = nameclass
-        Auto:
+import random
 
-    def __init__(self, brand):
-        self.brand = brand
-        self.passengers = []
+class Cat:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 50
+        self.energy = 50
 
-    def add_passenger(self, *args):
-        for passenger in args:
-            self.passengers.append(passenger)
+    def eat(self):
+        print(f"{self.name} поїв.")
+        self.hunger -= 20
+        self.energy += 10
 
-    def print_passengers_names(self):
-        if self.passengers != []:
-            print(f"Names of {self.brand} passengers: ")
-            for passenger in self.passengers:
-                print(passenger.name)
-        else:
-            print(f"There are no passengers in {self.brand}")
+    def sleep(self):
+        print(f"{self.name} поспав.")
+        self.energy += 30
 
+    def play(self):
+        print(f"{self.name} погрався.")
+        self.hunger += 20
+        self.energy -= 20
 
-nick = Human("Nick")
-kate = Human("Kate")
-car = Auto("Mercedes")
-car.add_passenger(nick, kate)
-car.print_passengers_names()
+    def live(self):
+        action = random.choice([self.eat, self.sleep, self.play])
+        action()
+        print(f"Статус -> Голод: {self.hunger}, Енергія: {self.energy}\n")
+
+my_cat = Cat("Барсік")
+
+for day in range(1, 4):
+    print(f"День {day}:")
+    my_cat.live()
